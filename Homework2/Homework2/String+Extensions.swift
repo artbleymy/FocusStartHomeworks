8f617523a15dda705e4857e352853ca86f47297d
Stanislav Kozlov <bleymy@mail.ru>
2019-11-08 20:31:28 +0300

    Реализация расширений для String

--- Homework2/Homework2/String+Extensions.swift
+++ Homework2/Homework2/String+Extensions.swift
@@ -22,25 +22,26 @@ import Foundation
 
 extension String
 {
-	func reversedWords() -> String {
-		let array = self.components(separatedBy: " ")
-		let result = String(array.map { String($0.reversed()) }.reduce("", { $0 + $1 + " " }).dropLast())
-		return result
-	}
+  func reversedWords() -> String {
+    let result = self.components(separatedBy: " ")
+      .map { String($0.reversed()) }
+      .reduce("", { $0 + $1 + " " })
+    return String(result.dropLast())
+  }
 
-	func validate() -> Bool {
-		let phoneNumber = self.replacingOccurrences(of: " ", with: "")
-		let pattern = "^(8|7|\\+7)[\\(]?(9)\\d{2}[\\)]?\\d{3}[\\-]?\\d{2}[\\-]?\\d{2}$"
-		do {
-			let regex = try NSRegularExpression(pattern: pattern,
-												options: .caseInsensitive)
-			let result = regex.firstMatch(in: phoneNumber,
-										  options: NSRegularExpression.MatchingOptions(rawValue: 0),
-										  range: NSRange(location: 0, length: phoneNumber.count)) != nil
-			return result
-		}
-		catch {
-			return false
-		}
-	}
+  func validate() -> Bool {
+    let phoneNumber = self.replacingOccurrences(of: " ", with: "")
+    let pattern = "^(8|7|\\+7)[\\(]?(9)\\d{2}[\\)]?\\d{3}[\\-]?\\d{2}[\\-]?\\d{2}$"
+    do {
+      let regex = try NSRegularExpression(pattern: pattern,
+                        options: .caseInsensitive)
+      let result = regex.firstMatch(in: phoneNumber,
+                      options: NSRegularExpression.MatchingOptions(rawValue: 0),
+                      range: NSRange(location: 0, length: phoneNumber.count)) != nil
+      return result
+    }
+    catch {
+      return false
+    }
+  }
 }
